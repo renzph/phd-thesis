aa32bed6084b9cfbd646812d5356c3062ee2b97a
Philipp <renz.ph@gmail.com>
2024-09-29 21:24:02 +0200

    After corrections

--- recompile_from_scratch.sh
+++ recompile_from_scratch.sh
@@ -4,7 +4,21 @@
 MAIN_TEX_FILE="main"
 
 # List of auxiliary file extensions to delete
-AUX_EXTENSIONS=("aux" "log" "out" "toc" "lof" "lot" "fls" "fdb_latexmk" "synctex.gz" "bbl" "blg" "nav" "snm" "vrb")
+AUX_EXTENSIONS=("aux" "log" "out" "toc" "lof" "lot" "fls" "fdb_latexmk" "synctex.gz" "bbl" "blg" "nav" "snm" "vrb" "log" "run.xml" "bcf")
+
+# Parse command line options
+DELETE_ONLY=false
+while getopts "d" opt; do
+    case ${opt} in
+        d )
+            DELETE_ONLY=true
+            ;;
+        \? )
+            echo "Usage: cmd [-d]"
+            exit 1
+            ;;
+    esac
+done
 
 # Delete auxiliary files
 for ext in "${AUX_EXTENSIONS[@]}"; do
@@ -14,10 +28,14 @@ for ext in "${AUX_EXTENSIONS[@]}"; do
     fi
 done
 
-# Recompile the LaTeX document
-pdflatex "$MAIN_TEX_FILE.tex"
-biber "$MAIN_TEX_FILE" # Only if you are using BibTeX for bibliography
-pdflatex "$MAIN_TEX_FILE.tex"
-pdflatex "$MAIN_TEX_FILE.tex"
+# If the -d option is not provided, recompile the LaTeX document
+if [ "$DELETE_ONLY" = false ]; then
+    pdflatex "$MAIN_TEX_FILE.tex"
+    biber "$MAIN_TEX_FILE" # Only if you are using BibTeX for bibliography
+    pdflatex "$MAIN_TEX_FILE.tex"
+    pdflatex "$MAIN_TEX_FILE.tex"
 
-echo "Recompilation completed."
+    echo "Recompilation completed."
+else
+    echo "Auxiliary files deleted. No recompilation performed."
+fi
